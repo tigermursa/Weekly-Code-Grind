b138b2bad0438f13cd643e31e1dfd965b2ca0a3e
Mursalin <mursalin2311@gmail.com>
2025-04-25 14:54:46 +0600

    first commit

---
+++ Array Methods.js
@@ -0,0 +1,7 @@
+/*
+Array Methods & Data Transformation:
+Given an array of user objects, 
+use map, filter, and reduce to compute the total age of all users 
+whose names start with the letter “A”.
+
+*/
